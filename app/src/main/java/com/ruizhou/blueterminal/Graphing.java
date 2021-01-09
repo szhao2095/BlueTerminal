3c66648bc15ed97ca15972fb05e7b37cf2016d39
MichaelYingEngineering <71140352+MichaelYingEngineering@users.noreply.github.com>
2021-01-08 21:04:29 -0500

    Change home page graph button to general data display button, which will lead to a page with two buttons (one for viewing file data and another for viewing graphs)

---
+++ app/src/main/java/com/ruizhou/blueterminal/Graphing.java
@@ -0,0 +1,119 @@
+package com.ruizhou.blueterminal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Graphing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graphing);
+
+        String filename = "data.txt";
+
+        // Read the file
+        FileInputStream fis = null;
+        String file_data = null;
+
+        try {
+            fis = openFileInput(filename);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+
+            String text;
+            while ((text = br.readLine()) != null) {
+                sb.append(text).append("\n");
+            }
+
+            file_data = sb.toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>();
+        LineGraphSeries<DataPoint> series2 = new LineGraphSeries<DataPoint>();
+
+        String[] separated = file_data.split("\n");
+        for (int i = 0; i < separated.length; i++) {
+            String line = separated[i].trim();
+
+            String[] sep = line.split(",");
+            String gas = sep[1].trim();
+            String distance = sep[2].trim();
+
+            int x = Integer.parseInt(sep[0].trim());
+            Double g = Double.parseDouble(gas);
+            Double d = Double.parseDouble(distance);
+            series.appendData(new DataPoint(x, g), true, separated.length);
+            series2.appendData(new DataPoint(x, d), true, separated.length);
+        }
+
+        series.setTitle("Gas Value (ADC)");
+        series.setColor(Color.GREEN);
+        series.setDrawDataPoints(true);
+        series.setDataPointsRadius(10);
+        series.setThickness(8);
+
+        graph.addSeries(series);
+
+        // set second scale
+        series2.setTitle("Distance (cm)");
+        series2.setColor(Color.RED);
+        series2.setDrawDataPoints(true);
+        series2.setDataPointsRadius(10);
+        series2.setThickness(8);
+
+        graph.getSecondScale().addSeries(series2);
+        // the y bounds are always manual for second scale
+        graph.getSecondScale().setMinY(0);
+        graph.getSecondScale().setMaxY(20);
+        graph.getSecondScale().setVerticalAxisTitle("Distance (cm)");
+        graph.getGridLabelRenderer().setVerticalLabelsSecondScaleColor(Color.RED);
+
+
+        graph.getLegendRenderer().setVisible(true);
+
+        // activate horizontal zooming and scrolling
+        graph.getViewport().setScalable(true);
+
+        // activate horizontal scrolling
+        graph.getViewport().setScrollable(true);
+
+        // activate horizontal and vertical zooming and scrolling
+        graph.getViewport().setScalableY(true);
+
+        // activate vertical scrolling
+        graph.getViewport().setScrollableY(true);
+
+        GridLabelRenderer gridLabel = graph.getGridLabelRenderer();
+        gridLabel.setHorizontalAxisTitle("Time (s)");
+        gridLabel.setVerticalAxisTitle("Gas Value (ADC)");
+    }
+}
