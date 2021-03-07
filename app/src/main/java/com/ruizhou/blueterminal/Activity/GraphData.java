3b48ef7effe14998c9b5c8bde535a23b6d3087f4
MichaelYingEngineering <71140352+MichaelYingEngineering@users.noreply.github.com>
2021-03-07 00:03:36 -0500

    Lay groundwork for saving files to external storage

--- app/src/main/java/com/ruizhou/blueterminal/Activity/GraphData.java
+++ app/src/main/java/com/ruizhou/blueterminal/Activity/GraphData.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,14 @@ import android.widget.Button;
 import com.ruizhou.blueterminal.Activity.DisplayData;
 import com.ruizhou.blueterminal.Activity.Graphing;
 import com.ruizhou.blueterminal.R;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GraphData extends AppCompatActivity {
 
@@ -51,7 +60,43 @@ public class GraphData extends AppCompatActivity {
         upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Upload file
+                // Create a copy of the file in EXTERNAL STORAGE
+                // Read the file
+                FileInputStream fis = null;
+                String file_data = null;
+
+                try {
+                    fis = openFileInput(filename);
+                    InputStreamReader isr = new InputStreamReader(fis);
+                    BufferedReader br = new BufferedReader(isr);
+                    StringBuilder sb = new StringBuilder();
+
+                    String text;
+                    while ((text = br.readLine()) != null) {
+                        Pattern endIndicator = Pattern.compile("END");
+                        Matcher m = endIndicator.matcher(text);
+                        if (m.find()) {
+                            continue;
+                        }
+                        sb.append(text).append("\n");
+                    }
+
+                    file_data = sb.toString();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (fis != null) {
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                Log.d("SANITYCONTENT", file_data);
+                boolean check = isExternalStorageWritable();
             }
         });
 
@@ -66,5 +111,14 @@ public class GraphData extends AppCompatActivity {
         Intent intent = new Intent(this, Graphing.class);
         intent.putExtra("filename", filename);
         startActivity(intent);
+    }
+
+    private boolean isExternalStorageWritable() {
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            Log.d("WRITABLE", "It is writable");
+            return true;
+        } else {
+            return false;
+        }
     }
 }
